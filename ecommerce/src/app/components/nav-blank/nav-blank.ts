6d81618b8b8121ee2e43a20687c35bec73954db7
ZiadtahaM <fagricastro@gmail.com>
2025-09-20 20:30:24 +0200

    Replace old project files with new ecommerce project structure

---
+++ ecommerce/src/app/components/nav-blank/nav-blank.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-blank',
+  standalone: false,
+  templateUrl: './nav-blank.html',
+  styleUrl: './nav-blank.css'
+})
+export class NavBlank {
+
+}
